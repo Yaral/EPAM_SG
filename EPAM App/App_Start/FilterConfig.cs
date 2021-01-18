0764e3d06b99a0b0721e4c0c9e9c80e1fa9c3d3b
Yara_Lopez <yaralopez17@gmail.com>
2021-01-17 19:51:35 -0700

    First porject Sync

---
+++ EPAM App/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EPAM_App
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
